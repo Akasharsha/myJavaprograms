1ae6352b9767053b0ffbb8106b384e5b0a036fdf
Akasharsha <35709093+Akasharsha@users.noreply.github.com>
2018-01-31 20:00:03 -0500

    ListSample

--- sample.java
+++ sample.java
@@ -1,42 +1,41 @@
-package training3;
+package training;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 
 public class sample {
-	
-	int i;
 
-	
-	public static void main(String[] args)  {
-		
-		
-		sample obj= new sample();
-		obj.m1();
-		
-	}
-	void m1()  {	
-		
-		try{
-			int a=10/0;
-		}
-		catch(ArithmeticException e){
-			System.out.println("AE exception handled");
-		}
+
+	   public static void main(String args[]){
+		  
+		   List list = new ArrayList();
+		   list.add("Akash");
+		   list.add("learning");
+		   list.add("java");
+		   list.add(new Exception());
+		   
+		   int size= list.size();
+		   System.out.println(size);
+		   
+//		   Object s= list.get(3);
+//		   System.out.println(s);
+		   for (int i = 0; i < list.size(); i++) {
+			   Object s=list.get(i);
+			   if(s instanceof String){
+				   String x=(String)s;
+				   System.out.println(x);
+			   }
+				   else if (s instanceof Object) {
+					   Object x=(Object) s;
+					   System.out.println(x);
+					
+				}
+	   }
 			
-	
 		}
-		
-	
-	protected void finalize() throws Throwable {
-		System.out.println("Cleaning started");
-	}
-		
-		
-		
-	
-		
-		
-	}
-	
+		  	   
+	  }
 	
